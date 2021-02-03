901c922f41eb936e48179d70a00089ea20cbc483
oksanakritskaya <sonce8881@mail.ru>
2021-02-03 16:51:39 +0700

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,13 @@ const gulp = require('gulp'),
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
+function html() {
+    return src('views/*.ejs')
+        .pipe(browserSync.stream());
+}
+
 function css() {
-    return src('public/scss/*.scss')
+    return src('src/stylesheets/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('styles.css'))
@@ -35,13 +40,39 @@ function js() {
         .pipe(browserSync.stream());
 }
 
+function images() {
+    return src(['src/images/*.*',  '!src/images/sprite.svg'])
+        .pipe(imagemin()).pipe(dest('public/images'))
+        .pipe(browserSync.stream());
+}
+
+function icons() {
+    return src('src/images/icons/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../sprite.svg"
+                }
+            },
+        }))
+        .pipe(dest('public/images'))
+        .pipe(browserSync.stream());
+}
+
+task('build',
+    parallel(images, icons)
+);
+
 task('watch', function () {
     browserSync.init({
-        port: 3002,                      //this can be any port, it will show our app
-        proxy: 'http://localhost:3000/', //this is the port where express server works
-        ui: {port: 3003},                //UI, can be any port
-        reloadDelay: 1000                //Important, otherwise syncing will not work
+        port: 3002,
+        proxy: 'http://localhost:3000/',
+        ui: {port: 3003},
+        reloadDelay: 1000
     });
+    watch('src/images/*.*', images);
+    watch('src/images/icons/*.*', icons);
+    watch('views/*.ejs', html);
     watch('public/javascripts/*.js', js);
-    watch('public/scss/*.scss', css).on('change', browserSync.reload);
+    watch('src/stylesheets/*.scss', css).on('change', browserSync.reload);
 });
